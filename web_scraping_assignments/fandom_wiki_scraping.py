b65cdcffe47857b0bf437e93154f6bbdca3fa750
GloriaLiu930 <156953911+GloriaLiu930@users.noreply.github.com>
2024-10-21 03:35:47 -0500

    Add files via upload

---
+++ web_scraping_assignments/fandom_wiki_scraping.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://harrypotter.fandom.com/wiki/List_of_deaths"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+tables = soup.find_all('table', class_='wikitable')
+
+table_data = []
+for table in tables:
+    headers = [header.text.strip() for header in table.find_all('th')]
+    rows = table.find_all('tr')
+
+    for row in rows:
+        cells = row.find_all(['td', 'th'])
+        row_data = [cell.text.strip() for cell in cells]
+        table_data.append(row_data)
+
+df = pd.DataFrame(table_data)
+df.to_csv('harry_potter_deaths.csv', index=False)
---
+++ web_scraping_assignments/first_web_scraping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.gutenberg.org/browse/scores/top"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+top_100_section = soup.find(id="books-last1")
+
+book_list = top_100_section.find_next('ol')
+
+book_items = book_list.find_all('li')
+
+for item in book_items:
+    title = item.get_text().strip()
+    print(title)
